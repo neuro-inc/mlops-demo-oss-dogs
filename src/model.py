96fe9eec1eb052f0c7d95f173d209417596d4234
Yevhenii Semendiak <semendyak@gmail.com>
2021-05-17 20:00:49 +0300

    add explainer, fix dependencies and model architecture

--- src/model.py
+++ src/model.py
@@ -1,18 +1,26 @@
+from config.preprocessing import INPUT_SIZE
 from keras import models, layers, optimizers
 
+from keras.applications import VGG16
 from config.model import INPUT_LAYER_SHAPE
 
 
-def get_model() -> models.Sequential:
-    # Define fully connected feed-forward model and its hyperparameters
+def get_model() -> models.Model:
     model = models.Sequential()
+    encoder = VGG16(weights="imagenet", include_top=False, input_shape=(*INPUT_SIZE, 3))
+    encoder.trainable = False
+    model.add(encoder)
     model.add(layers.Flatten(input_shape=INPUT_LAYER_SHAPE))
     model.add(layers.Dense(256, activation="relu", input_shape=INPUT_LAYER_SHAPE))
-    model.add(layers.Dense(1, activation="sigmoid"))
-    model.summary()
+    model.add(layers.Dense(2, activation="softmax"))
 
     # Compile model
     model.compile(
-        optimizer=optimizers.Adam(), loss="binary_crossentropy", metrics=["acc"]
+        optimizer=optimizers.Adam(),
+        loss="sparse_categorical_crossentropy",
+        metrics=["acc"],
     )
+
+    model.summary()
+
     return model
