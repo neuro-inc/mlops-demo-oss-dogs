14313fd42ea643608958010596142dab2315232d
Alexey Naiden <alexey.nayden@gmail.com>
2022-02-16 12:46:17 +0300

    Update LS

--- label_studio/launch_ls.py
+++ label_studio/launch_ls.py
@@ -45,13 +45,14 @@ async def _run_label_studio(
                   headers=AUTH)
 
     existing_results_file = project_root / "data" / "result.json"
-    # Append existing tasks from JSON
-    ls_client = Client(url="http://localhost:443", api_key="token12356")
-    project = Project.get_from_id(client=ls_client, project_id=1)
-    project.import_tasks(tasks=existing_results_file.absolute())
-    # requests.post(url=f"http://localhost:443/api/projects/1/import",
-    #               headers=AUTH,
-    #               data=existing_results_file.read_bytes())
+    if existing_results_file.exists():
+        # Append existing tasks from JSON
+        ls_client = Client(url="http://localhost:443", api_key="token12356")
+        project = Project.get_from_id(client=ls_client, project_id=1)
+        project.import_tasks(tasks="/usr/project/data/result.json")
+        # requests.post(url=f"http://localhost:443/api/projects/1/import",
+        #               headers=AUTH,
+        #               data=existing_results_file.read_bytes())
 
     # Sync tasks from local storage
     requests.post(url=f"http://localhost:443/api/storages/localfiles/1/sync",
