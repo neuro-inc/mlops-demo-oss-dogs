cd8da28396436375a383b1fd5488b7f2ede31570
Alexey Naiden <alexey.nayden@gmail.com>
2022-02-18 13:40:25 +0300

    Update LS

--- label_studio/launch_ls.py
+++ label_studio/launch_ls.py
@@ -32,46 +32,46 @@ async def _run_label_studio(
     logging.info('Starting Label Studio...')
     # use start_new_session=True not to send KeyboardInterrupt to subprocess
     ls_proc = await asyncio.create_subprocess_exec(*cmd, start_new_session=True)
-    await asyncio.sleep(10)
+    # await asyncio.sleep(10)
 
-    logging.info("Creating local storage via Label Studio API")
-    # Create local storage
-    requests.post(url=f"http://localhost:443/api/storages/localfiles",
-                  json={
-                      "project": 1,
-                      "title": "Pachyderm",
-                      "path": "/usr/project/data/Images/",
-                      "use_blob_urls": True
-                  },
-                  headers=AUTH)
+    # logging.info("Creating local storage via Label Studio API")
+    # # Create local storage
+    # requests.post(url=f"http://localhost:443/api/storages/localfiles",
+    #               json={
+    #                   "project": 1,
+    #                   "title": "Pachyderm",
+    #                   "path": "/usr/project/data/Images/",
+    #                   "use_blob_urls": True
+    #               },
+    #               headers=AUTH)
 
-    existing_results_file = project_root / "data" / "result.json"
-    existing_files = []
-    # Append existing tasks from JSON
-    ls_client = Client(url="http://localhost:443", api_key="token12356")
-    project = Project.get_from_id(client=ls_client, project_id=1)
+    # existing_results_file = project_root / "data" / "result.json"
+    # existing_files = []
+    # # Append existing tasks from JSON
+    # ls_client = Client(url="http://localhost:443", api_key="token12356")
+    # project = Project.get_from_id(client=ls_client, project_id=1)
 
-    if existing_results_file.exists():
-        # Get existing files
-        existing_tasks = json.loads(existing_results_file.read_text())
-        existing_files = [task['data']['image'].split('/')[-1] for task in existing_tasks]
-        project.import_tasks(tasks="/usr/project/data/result.json")
+    # if existing_results_file.exists():
+    #     # Get existing files
+    #     existing_tasks = json.loads(existing_results_file.read_text())
+    #     existing_files = [task['data']['image'].split('/')[-1] for task in existing_tasks]
+    #     project.import_tasks(tasks="/usr/project/data/result.json")
 
-    # Import new images
-    all_images_root = project_root / "data" / "Images"
-    new_tasks = []
-    for img in all_images_root.glob('*'):
-        if img.name in existing_files:
-            continue
-        new_task = {
-            'data': {
-                'image': f"/data/local-files/?d={str(img)[1:]}"
-            }
-        }
-        new_tasks.append(new_task)
-    print(f'new tasks: {new_tasks}')
-    if len(new_tasks) > 0:
-        project.import_tasks(tasks=new_tasks)
+    # # Import new images
+    # all_images_root = project_root / "data" / "Images"
+    # new_tasks = []
+    # for img in all_images_root.glob('*'):
+    #     if img.name in existing_files:
+    #         continue
+    #     new_task = {
+    #         'data': {
+    #             'image': f"/data/local-files/?d={str(img)[1:]}"
+    #         }
+    #     }
+    #     new_tasks.append(new_task)
+    # print(f'new tasks: {new_tasks}')
+    # if len(new_tasks) > 0:
+    #     project.import_tasks(tasks=new_tasks)
 
     # Sync tasks from local storage
     # requests.post(url=f"http://localhost:443/api/storages/localfiles/1/sync",
@@ -120,6 +120,7 @@ def _save_labeling_results(project_root: Path) -> None:
     logging.info(f"Saving results to {results_file}")
     with results_file.open("wb") as fd:
         fd.write(response.content)
+
 
 
 def get_args() -> argparse.Namespace:
